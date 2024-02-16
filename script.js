eb9a882ff9b38c0546cb1b761101d2c5559243bc
harshrao26 <harshurao058@gmail.com>
2024-02-16 14:53:31 +0530

    Desktop View Completed

---
+++ script.js
@@ -0,0 +1,36 @@
+document.querySelector(".ser-1").addEventListener("mousemove", function(){
+    document.querySelector(".h-div-1").style.top = "0%";
+    document.querySelector(".ser-1").style.cursor = "pointer";
+})
+
+document.querySelector(".ser-1").addEventListener("mouseleave", function(){
+    document.querySelector(".h-div-1").style.top = "100%";
+})
+
+
+document.querySelector(".ser-2").addEventListener("mousemove", function(){
+    document.querySelector(".h-div-2").style.top = "0%";
+    document.querySelector(".ser-2").style.cursor = "pointer";
+})
+
+document.querySelector(".ser-2").addEventListener("mouseleave", function(){
+    document.querySelector(".h-div-2").style.top = "100%";
+})
+
+document.querySelector(".ser-3").addEventListener("mousemove", function(){
+    document.querySelector(".h-div-3").style.top = "0%";
+    document.querySelector(".ser-3").style.cursor = "pointer";
+})
+
+document.querySelector(".ser-3").addEventListener("mouseleave", function(){
+    document.querySelector(".h-div-3").style.top = "100%";
+})
+
+document.querySelector(".ser-4").addEventListener("mousemove", function(){
+    document.querySelector(".h-div-4").style.top = "0%";
+    document.querySelector(".ser-4").style.cursor = "pointer";
+})
+
+document.querySelector(".ser-4").addEventListener("mouseleave", function(){
+    document.querySelector(".h-div-4").style.top = "100%";
+})
